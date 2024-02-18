3d12a2b166756e6edfde6649856f6eaeb1362ccd
N0Uchan <142255222+N0Uchan@users.noreply.github.com>
2024-02-18 15:39:53 +0530

    mm

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,6 +1,7 @@
 import ghIcon from '../assets/gh.svg';
 import ghDark from '../assets/ghd.svg';
 export default function Btn({btn}) {
+     
     if ( btn === 'gh'){
         btn = <a href='https://github.com/N0Uchan/calculator' ><img src={ghDark} alt='github' id='ghIcon'  /></a> 
 
