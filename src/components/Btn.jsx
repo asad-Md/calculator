fbe561af0024e2c8b709f7098a143ad79c57f99f
N0Uchan <142255222+N0Uchan@users.noreply.github.com>
2024-02-18 18:58:57 +0530

    util js

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,7 +1,7 @@
 import ghIcon from '../assets/gh.svg';
 import ghDark from '../assets/ghd.svg';
 export default function Btn({btn}) {
-     
+
     if ( btn === 'gh'){
         btn = <a href='https://github.com/N0Uchan/calculator' ><img src={ghDark} alt='github' id='ghIcon'  /></a> 
 
@@ -11,3 +11,7 @@ export default function Btn({btn}) {
 
 
 //diff button for == / Clear
+//onclick
+
+//string.append
+//if +-*,,, string[-1] = new operator
---
+++ src/util/calcs.js
@@ -0,0 +1,28 @@
+export function calculate(numArr, opsArray) {
+    const operations = ['^', '*', '/', '+', '-'];
+
+    const applyOperation = (a, b, op) => {
+        switch (op) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/': return a / b;
+            case '^': return a ** b;
+            case '%': return a % b;
+            default: throw new Error('Unknown operation' + op);
+        }
+    };
+
+    for (let op of operations) {
+        while (opsArray.includes(op)) {
+            let i = opsArray.indexOf(op);
+            numArr[i] = applyOperation(numArr[i], numArr[i + 1], op);
+            numArr.splice(i + 1, 1);
+            opsArray.splice(i, 1);
+        }
+    }
+
+    return numArr[0];
+}
+
+
