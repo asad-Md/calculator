7f1c0f330963ac527006922a7b0613fcd242f2b4
N0Uchan <142255222+N0Uchan@users.noreply.github.com>
2024-02-18 12:33:28 +0530

    make btns component

---
+++ src/components/Btn.jsx
@@ -0,0 +1,6 @@
+
+
+export default function Btn({btn}) {
+    
+    return <div class='numBtn innBtns'>{btn}</div>
+}
--- src/components/BtnsContainer.jsx
+++ src/components/BtnsContainer.jsx
@@ -1,25 +1,24 @@
-import './Btns.css';
-
+import "./Btns.css";
+import Btn from "./Btn";
 export default function Btns() {
-    return (
-        <div id='btnsCont'>
-          
-          <div class='numBtn innBtns btn1'>1</div>
-          <div class='numBtn innBtns edgeTop'>2</div>
-          <div class='numBtn innBtns edgeTop'>3</div>
-          <div class='symBtn innBtns btn+'>+</div>
-          <div class='numBtn innBtns edge'>4</div>
-          <div class='numBtn innBtns mid'>5</div>
-          <div class='numBtn innBtns mid'>6</div>
-          <div class='symBtn innBtns edge'>-</div>
-          <div class='numBtn innBtns edge'>7</div>
-          <div class='numBtn innBtns mid'>8</div>
-          <div class='numBtn innBtns mid'>9</div>
-          <div class='symBtn innBtns edge'>x</div>
-          <div class='numBtn innBtns corner'>0</div>
-          <div class='symBtn innBtns edge'>.</div>
-          <div class='symBtn innBtns edge'>=(or)C</div>
-          <div class='symBtn innBtns corner'>/</div>
-        </div>
-    )}
-        
+  return (
+    <div id='btnsCont'>
+      <Btn btn={1} />
+      <Btn btn={2} />
+      <Btn btn={3} />
+      <Btn btn={"+"} />
+      <Btn btn={4} />
+      <Btn btn={5} />
+      <Btn btn={6} />
+      <Btn btn={"-"} />
+      <Btn btn={7} />
+      <Btn btn={8} />
+      <Btn btn={9} />
+      <Btn btn={"*"} />
+      <Btn btn={"."} />
+      <Btn btn={0} />
+      <Btn btn={"="} />
+      <Btn btn={"/"} />
+    </div>
+  );
+}
